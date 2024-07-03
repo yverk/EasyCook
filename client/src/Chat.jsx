dcf78d124375e69e5e92df1a0d156f265c5e682e
Dahiver Guerra <95004742+Dreamzur@users.noreply.github.com>
2024-07-03 15:11:55 -0400

    Added chat page

---
+++ client/src/Chat.jsx
@@ -0,0 +1,33 @@
+export default function Chat() {
+  return (
+    <div className="flex h-screen">
+      <div className="bg-white w-1/3">Tabs</div>
+      <div className="flex flex-col bg-blue-100 w-2/3 p-2">
+        <div className="flex-grow">Messages</div>
+        <div className="flex gap-2 mx-2">
+          <input
+            type="text"
+            placeholder="Type your message here"
+            className="bg-white flex-grow border p-2"
+          />
+          <button className="bg-blue-500 p-2 text-white rounded-sm">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import { UserContext } from "./UserContext";
+import Chat from "./Chat";
 
 export default function Routes() {
   const {username, id} = useContext(UserContext);
   
   if (username) {
-    return 'Currently logged in as ' + username + "!";
+    return <Chat />;
   }
   return (
     <RegisterAndLoginForm />
